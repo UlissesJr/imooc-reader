a28f7658cbb347aa5c86d68843a09d3c5477e3fa
ganfuwen <liuzhiyu@cestc.cn>
2021-01-28 17:51:54 +0800

    整合MyBatis-plus

--- src/main/java/com/imooc/reader/mapper/TestMapper.java
+++ src/main/java/com/imooc/reader/mapper/TestMapper.java
@@ -1,6 +1,10 @@
 package com.imooc.reader.mapper;
 
-public interface TestMapper {
-    public void insert();
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.imooc.reader.entity.Test;
+
+// BaseMapper<Test> 其中Test指的是entity中的Test实体
+public interface TestMapper extends BaseMapper<Test> {
+    public void insertSample();
 
 }
